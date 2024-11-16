250301d3450a75838b8dd6494665b35bc82472e9
vince072618 <vincentroycamba072618@gmail.com>
2024-11-16 20:21:30 +0800

    added channel name

--- dashboard.php
+++ dashboard.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Alcohol POS Dashboard</title>
+    <title>COMPUTER POS Dashboard</title>
     <style>
         /* Global styles */
         body {
